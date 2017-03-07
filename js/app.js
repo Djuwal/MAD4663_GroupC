b294f1abd46d5a99cc22a1b88543a4acb8744067
Juan Ramirez <juanramirez@mail.usf.edu>
2017-03-07 14:39:19 -0500

    JC added the DB that works on the Social page.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,13 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services',])
+
+// JC ADDED
+var db=null;
+//var app=angular.module('starter', ['ionic','app.controllers', 'app.routes', 'app.directives','app.services','ngCordova']);
+
+// JC ADDED ngCordova
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services', 'ngCordova'])
 
 .config(function($ionicConfigProvider, $sceDelegateProvider){
   
@@ -14,7 +20,7 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
 
 })
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaSQLite) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -26,8 +32,61 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+
+    // JC ADDED
+    db=window.openDatabase("sqlite", "1.0", "sqlitedemo",2000);
+    $cordovaSQLite.execute(db, "CREATE TABLE forumDB(id integer primary key, comment text)");
+    $cordovaSQLite.execute(db, "CREATE TABLE coachDB(id integer primary key, comment text)");
+
   });
 })
+
+
+
+// JC ADDED controller
+// *****************************
+.controller('infoCtrl', function($scope, $cordovaSQLite, $interval){
+
+
+
+  $scope.addInfo=function(){
+    var query="INSERT INTO forumDB(comment) VALUES (?)";
+    $cordovaSQLite.execute(db,query,[$scope.comment]);
+    $scope.load();
+    //$scope.form.$setPristine();
+    $scope.comment = '';
+  }
+  $scope.load=function(){
+    $scope.alldata=[];
+    $cordovaSQLite.execute(db,"SELECT id, comment FROM forumDB ORDER BY id DESC").then(function(result){
+      if(result.rows.length){
+        for (var i=0; i < result.rows.length; i++){
+          $scope.alldata.push(result.rows.item(i));
+        }
+      } else {
+        console.log("No data found");
+      }
+    }, function(error){
+      console.log("error" + err);
+    });
+  }
+
+  // Run once. Load the data from the database.
+  $interval(callAtStart, 500, 1);
+
+  function callAtStart() {
+    $scope.load();
+  }
+
+
+
+})
+
+// End JC Controller.
+
+
+
+
 
 /*
   This directive is used to disable the "drag to open" functionality of the Side-Menu
